b2fbf11d9f8d20d146bbc25dc8a45cf1ebfdfc79
Jouni K. Seppänen <jks@iki.fi>
2024-08-07 18:19:53 +0300

    README ja uusi versionumero
    GPL:n vaatimus on, että muutokset merkitään näkyvästi. Lisäsin myös
    Tietoja-näkymään viittauksen lisenssiin, joka oli aiemmin vain
    GitHubissa COPYING-tiedostona.
    
    Erinäisiä forkkeja näkyy olevan, joten laitoin versionumeroon
    tunnisteeksi GitHub-käyttäjänimeni.

---
+++ README.md
@@ -0,0 +1,40 @@
+# Tilitin
+
+Tilitin on ilmainen kirjanpito-ohjelma. Ohjelman teki Tommi Helineva, ja sen dokumentaatio on hänen
+sivuillaan: https://helineva.net/tilitin/
+
+Ohjelmaan on vuonna 2024 tehnyt pieniä ajanmukaistuksia ja korjauksia Jouni Seppänen.
+
+## Asennus
+
+1. Asenna Java 21 tai uudempi. Suosittelen OpenJDK-versiota, joka on saatavailla ilmaiseksi ja
+   avoimella lisenssillä. Esimerkiksi [Azulin](https://www.azul.com/downloads/#zulu) tai
+   [Adoptiumin](https://adoptium.net/) jakelut ovat hyviä. (Oraclen Javaa en suosittele, koska
+   vaikka uusia versioita saakin ilmaiseksi, sillä on ollut kummallisia lisenssitemppuja
+   menneisyydessä.)
+2. Lataa jar-tiedosto GitHubista ja kopioi sopivaan hakemistoon.
+3. Tuplaklikkaa jar-tiedostoa. Jos sinulla on Mac, tämä johtaa luultavasti valitukseen
+   epäilyttävästä ohjelmistosta. Jos uskallat käyttää sitä, voit ohittaa valituksen klikkaamalla
+   jar-tiedostoa hiiren oikealla painikkeella (ohjauslevyllä käytä kahta sormea, tai paina
+   ctrl-näppäintä klikatessa) ja valitsemalla "Avaa".
+
+## Muutokset
+
+- Tilittimen pitäisi nyt toimia uusillakin Maceilla, joissa on ARM-siru.
+- Korjattu Macilla esiintynyt bugi, jossa tekstikentän ensimmäinen merkki katosi usein.
+- Tilikartat ovat nyt jar-paketin sisällä, joten niitä ei tarvitse kopioida erikseen oikeaan
+  hakemistoon.
+- Uusi tilikartta alv-prosentilla 25,5%
+- Pieniä teknisiä ajanmukaistuksia
+
+## Lisenssi
+
+Tämä on vapaa ohjelma: tätä ohjelmaa saa levittää edelleen ja muuttaa Free Software Foundationin
+julkaiseman GNU General Public Licensen (GPL-lisenssi) version 3 ehtojen mukaisesti.
+
+Tätä ohjelmaa levitetään siinä toivossa, että se olisi hyödyllinen mutta ilman mitään takuuta; edes
+hiljaista takuuta kaupallisesti hyväksyttävästä laadusta tai soveltuvuudesta tiettyyn tarkoitukseen.
+Katso GPL-lisenssistä lisää yksityiskohtia.
+
+Tämän ohjelman mukana pitäisi tulla kopio GPL-lisenssistä tiedostossa COPYING. Jos näin ei ole,
+katso <http://www.gnu.org/licenses/>.
--- src/main/java/kirjanpito/ui/AboutDialog.java
+++ src/main/java/kirjanpito/ui/AboutDialog.java
@@ -12,6 +12,8 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
 
 import kirjanpito.ui.resources.Resources;
 
@@ -27,14 +29,14 @@ public class AboutDialog extends JDialog {
 		super(owner, "Tietoja ohjelmasta " +
 				Kirjanpito.APP_NAME, true);
 	}
-	
+
 	/**
 	 * Luo ikkunan komponentit.
 	 */
 	public void create() {
 		setLayout(new GridBagLayout());
 		setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
-		
+
 		GridBagConstraints c = new GridBagConstraints();
 
 		ImageIcon icon = new ImageIcon(Resources.loadAsImage("tilitin-48x48.png"));
@@ -43,29 +45,57 @@ public class AboutDialog extends JDialog {
 		c.weightx = 1.0;
 		c.weighty = 0.5;
 		add(new JLabel(icon), c);
-		
+
 		JLabel label;
-		
+
 		label = new JLabel("<html><big><b>" +
 				Kirjanpito.APP_NAME + " " + Kirjanpito.APP_VERSION +
 				"</b></big></html>");
-		
+
 		c.gridy = 1;
 		c.insets = new Insets(5, 40, 5, 40);
 		c.weighty = 0.0;
 		add(label, c);
-		
+
 		label = new JLabel("Kirjanpito-ohjelma");
 		c.gridy = 2;
 		c.insets = new Insets(5, 10, 5, 10);
 		add(label, c);
-		
-		label = new JLabel(
-			"<html><small>© 2009–2013 Tommi Helineva</small></html>");
-		
+
+		JTextPane textPane = new JTextPane();
+		textPane.setContentType("text/html");
+		textPane.setText(
+				"""
+							<html>
+							<body style='width: 300px; word-wrap: break-word;'>
+							<p>© 2009–2013 Tommi Helineva<br>
+							© 2024 Jouni Seppänen</p>
+							<p>Tämä on vapaa ohjelma: tätä ohjelmaa saa levittää edelleen ja muuttaa
+							Free Software Foundationin julkaiseman GNU General Public Licensen
+							(GPL-lisenssi) version 3 ehtojen mukaisesti.</p>
+							<p>Tätä ohjelmaa levitetään siinä toivossa, että se olisi hyödyllinen
+							mutta ilman mitään takuuta; edes hiljaista takuuta kaupallisesti hyväksyttävästä
+							laadusta tai soveltuvuudesta tiettyyn tarkoitukseen. Katso GPL-lisenssistä
+							lisää yksityiskohtia.</p>
+							<p>Tämän ohjelman mukana pitäisi tulla kopio GPL-lisenssistä tiedostossa
+							COPYING. Jos näin ei ole, katso<br>
+							http://www.gnu.org/licenses/.</p>
+							</body>
+							</html>
+						""");
+		textPane.setEditable(false);
+		textPane.setBorder(null);
+
+		JScrollPane scrollPane = new JScrollPane(textPane);
+		scrollPane.setPreferredSize(new Dimension(400, 380));
+		scrollPane.setBorder(null);
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
 		c.gridy = 3;
-		add(label, c);
-		
+		c.fill = GridBagConstraints.BOTH;
+		add(scrollPane, c);
+
 		JButton closeButton = new JButton("Sulje",
 				new ImageIcon(Resources.loadAsImage("close-22x22.png")));
 		closeButton.setMnemonic('S');
@@ -75,13 +105,13 @@ public class AboutDialog extends JDialog {
 				dispose();
 			}
 		});
-		
+
 		c.anchor = GridBagConstraints.LAST_LINE_END;
 		c.gridy = 4;
 		c.insets = new Insets(10, 10, 10, 10);
 		c.weighty = 0.5;
 		add(closeButton, c);
-		
+
 		getRootPane().setDefaultButton(closeButton);
 		pack();
 		setLocationRelativeTo(getOwner());
